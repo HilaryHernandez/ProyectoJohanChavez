4e665101215452f8b3130e985682a4bdbf7cae7d
VYRUXAR <152411025+VYRUXAR@users.noreply.github.com>
2023-12-06 18:37:23 -0500

    Tafur2

---
+++ CL_Controlador/CalcularSueldo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL_Controlador
+{
+    public class CalcularSueldo
+    {
+        public static int CalcularIbc(int sueldo, int horasEx)
+        {
+            int valorHoraNormal = 6000;
+
+            int valorHoraEx = (((valorHoraNormal*25)/100) +valorHoraNormal)*horasEx;
+            int resultado = ((sueldo + valorHoraEx)*40)/100;
+
+            return resultado;
+
+        }
+    }
+}
